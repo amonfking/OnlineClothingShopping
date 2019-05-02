b44587270b7b0a2af267698d51af0514c9e31f34
Ascol9861 <ascolparajuli65@gmail.com>
2019-05-02 20:20:05 +0545

    Login and Register layout completed

--- app/src/main/java/com/example/onlineclothingshopping/ViewPagerActivity.java
+++ app/src/main/java/com/example/onlineclothingshopping/ViewPagerActivity.java
@@ -1,6 +1,7 @@
 package com.example.onlineclothingshopping;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,16 +10,15 @@ import adapter.ViewPagerAdapter;
 import fragments.RegisterFragment;
 import fragments.loginFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewPagerActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_pager);
 
         tabLayout = findViewById(R.id.tabId);
         viewPager = findViewById(R.id.viewPager);
@@ -31,5 +31,4 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
     }
-
 }
